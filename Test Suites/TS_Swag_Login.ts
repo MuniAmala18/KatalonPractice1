9c7c079ecb2ed11a94dab17791e12c1d02fc8cae
VH427KA <VH427KA@IN3554302W1.ey.net>
2024-04-21 12:59:25 +0530

    TestSuite login SwagLabs

---
+++ Test Suites/TS_Swag_Login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Swag_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bc018b82-d789-4485-b8e8-a505124b4336</testSuiteGuid>
+   <testCaseLink>
+      <guid>c1f8bb06-6f0e-4aed-b616-ca576972860b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
