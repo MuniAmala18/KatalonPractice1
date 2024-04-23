6e0a1dced99826d580892239be2842365ace0a66
VH427KA <VH427KA@IN3554302W1.ey.net>
2024-04-23 22:01:45 +0530

    pet commit 3

---
+++ Test Suites/Jpet/TS_Pet_Signout.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Pet_Signout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1c5b00ab-8edc-41a4-9960-bb8c3383ab61</testSuiteGuid>
+   <testCaseLink>
+      <guid>2192b781-d37c-4f1b-9102-e609f3f451a3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Jpet/signout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
