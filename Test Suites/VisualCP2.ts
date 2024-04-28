0a154c62efc94587387493140def5012d2e00ae6
VH427KA <VH427KA@IN3554302W1.ey.net>
2024-04-28 12:47:03 +0530

    Visual checkpoint1

---
+++ Test Suites/VisualCP2.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>VisualCP2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4a33f534-379b-46d8-964d-1162601039cb</testSuiteGuid>
+   <testCaseLink>
+      <guid>1a302d8e-091b-4015-95fc-41ea67a85004</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Visual_Testing/VisualTesting2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
