529bb96cabff8c3b521b8092d73b6510e7440a9f
VH427KA <VH427KA@IN3554302W1.ey.net>
2024-04-28 12:53:22 +0530

    Visual commit 1

---
+++ Test Suites/VisualCP3.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>VisualCP3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e122be10-2279-41fa-9db3-6a11bf1308e9</testSuiteGuid>
+   <testCaseLink>
+      <guid>dc0ab2a3-e4a4-4990-bf31-7dc364ae76f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Visual_Testing/VisualTesting3</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
